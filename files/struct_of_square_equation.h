4c04629223a4c13270e1a130d2028625e6fbba76
batyarampage <137216948+batyarampage@users.noreply.github.com>
2023-08-23 15:44:04 +0300

    Create struct_of_square_equation.h

---
+++ files/struct_of_square_equation.h
@@ -0,0 +1,15 @@
+#ifndef struct_of_square_equation
+#define struct_of_square_equation
+///Структура, указывающая на коэффициенты квадратного уравнения
+struct square_equation_coefs{
+    /// коэффициент a
+    double a = 0;
+
+    /// коэффициент b
+    double b = 0;
+
+    /// коэффициент c
+    double c = 0;
+};
+
+#endif
