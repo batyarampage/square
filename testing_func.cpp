21d5fed4ed309a9a415de7b04247bb7b2fb28b59
batyarampage <137216948+batyarampage@users.noreply.github.com>
2023-08-30 12:56:19 +0300

    до этого коммита было так: сначала считывание из файла в массив структур, а затем тестирование. Теперь: при считывании теста он сразу же тестируется

--- testing_func.cpp
+++ testing_func.cpp
@@ -11,46 +11,56 @@ const char* RED = "\033[31m";
 const char* WHITE = "\033[0m";
 const char* GREEN = "\033[32m";
 const char* PURPLE = "\033[3;35m";
+const int FSCANF_COUNT_FROM_FILE = 6;
 
-void testing (struct test_square_coefs all_test[]){
+void testing (){
 
-    assert(all_test != nullptr);
+    FILE *fle = fopen("tests.txt", "r");
 
-    bool ok_test = true;
+    if (fle == nullptr){
 
-    read_from_file (all_test, &ok_test);
+        printf("Возможно, вы удалили файл с тестовыми условиями, тестов не будет\n");
 
-    int done_tests = 0;
+        cleaner_stdio ();
 
-    if (ok_test){
+    }
 
-        for (int i = 0; i < TEST_COUNT; i++){
+    else{
+
+        int done_tests = 0;
+        int count_of_all_tests = 0;
+
+        double a = 0;
+        double b = 0;
+        double c = 0;
+        int roots_quantity = 0;
+        double x1ref = 0;
+        double x2ref = 0;
+
+        while ((fscanf(fle, "%lg%lg%lg%d%lg%lg", &a, &b, &c, &roots_quantity, &x1ref, &x2ref)) == FSCANF_COUNT_FROM_FILE){
+
+            count_of_all_tests++;
 
             struct square_equation_coefs local_coef;
             struct roots_square_equation local_roots;
 
-            local_coef.a = all_test[i].a;
-            local_coef.b = all_test[i].b;
-            local_coef.c = all_test[i].c;
+            local_coef.a = a;
+            local_coef.b = b;
+            local_coef.c = c;
+
+            Count_of_roots count_of_roots_ref = Count_of_roots (roots_quantity);
 
             Type_of_equation type_of_input_equation = type_of_equation_function (&local_coef);
 
             Count_of_roots count_root1 = solving_equation (&local_coef, type_of_input_equation, &local_roots);
 
-            double x1ref = all_test[i].x1r;
-            double x2ref = all_test[i].x2r;
-            Count_of_roots count_of_roots_ref = all_test[i].count_root;
+            double local_root_x1 = local_roots.x1;
+            double local_root_x2 = local_roots.x2;
 
-            double local_rootx1 = local_roots.x1;
-            double local_rootx2 = local_roots.x2;
+            if ((compare_two_double_in_test(local_root_x2, x2ref)) || (compare_two_double_in_test(local_root_x1, x1ref)) || (count_of_roots_ref != count_root1)){
 
-            if ((count_of_roots_ref != count_root1) ||
-                (compare_two_double_in_test (x1ref, local_rootx1)) ||
-                (compare_two_double_in_test (x2ref, local_rootx2))){
-
-                printf("%s NOT PASSED TEST %d, x1ref = %g, x1 = %g, x2ref = %g, x2 = %g, количество корней ожидаемое = %d, реальное количество корней = %d %s\n ", RED,
-                        i+1, x1ref, local_roots.x1,x2ref,local_roots.x2, count_of_roots_ref, count_root1, WHITE);
-
+                     printf("%s NOT PASSED TEST %d, x1ref = %g, x1 = %g, x2ref = %g, x2 = %g, количество корней ожидаемое = %d, реальное количество корней = %d %s\n ", RED,
+                        count_of_all_tests, x1ref, local_root_x1,x2ref,local_root_x2, count_of_roots_ref, count_root1, WHITE);
             }
 
             else {
@@ -60,16 +70,11 @@ void testing (struct test_square_coefs all_test[]){
             }
         }
 
+        fclose(fle);
+
         printf("%s Корректно тестов = %d %s\n ", PURPLE,done_tests, WHITE);
-        printf("%s Некорректно тестов = %d %s \n", PURPLE, TEST_COUNT-done_tests, WHITE);
+        printf("%s Некорректно тестов = %d %s \n", PURPLE, count_of_all_tests-done_tests, WHITE);
         printf("\n");
-        //Выше сделан перевод строки, чтобы функция проверки ввода не считала этот пробел, я сразу же читаю его
-                  // из потока ввода
-    }
-
-    else{
-
-        printf("файл не существует\n");
     }
 }
 
@@ -85,45 +90,6 @@ bool compare_two_double_in_test (double num1, double num2){
     return true;
 }
 
-void read_from_file (struct test_square_coefs all_test[], bool *ok_test){
-
-    FILE *fle = fopen("tests.txt", "r");
-
-    if (fle == nullptr){
-
-        printf("Возможно, вы удалили файл с тестовыми условиями, тестов не будет\n");
-        *ok_test = false;
-
-        cleaner_stdio ();
-
-    }
-    else {
-
-        for (int i = 0; i<TEST_COUNT;i++){
-
-            double a = 0;
-            double b = 0;
-            double c = 0;
-            int roots_quantity = 0;
-            double x1ref = 0;
-            double x2ref = 0;
-
-            fscanf(fle, "%lg%lg%lg%d%lg%lg", &a, &b, &c, &roots_quantity, &x1ref, &x2ref);
-
-            Count_of_roots count_roots1 = Count_of_roots (roots_quantity);
-
-            all_test[i].a = a;
-            all_test[i].b = b;
-            all_test[i].c = c;
-            all_test[i].count_root = count_roots1;
-            all_test[i].x1r = x1ref;
-            all_test[i].x2r = x2ref;
-        }
-
-        fclose(fle);
-    }
-}
-
 void greeting_user_for_test (){
 
     printf("Если хотите прогнать unit-тесты, то введите букву t, иначе любое другое число или букву\n");
@@ -135,9 +101,7 @@ void is_user_wanna_test (){
 
     if (check_user_input()){
 
-        test_square_coefs all_test[TEST_COUNT] = {{}};
-
-        testing (all_test);
+        testing ();
     }
 
     else{
--- testing_func.h
+++ testing_func.h
@@ -18,14 +18,13 @@ bool compare_two_double_in_test (double num1, double num2);
  \param ok_test - указатель на булеву переменную для проверки существования файла тестирования
 
 */
-void read_from_file (struct test_square_coefs all_test[], bool *ok_test);
 
 
 ///Функция тестирования основной программы
 /*!
  \param all_test - массив структур для заполнения из файла
 */
-void testing (struct test_square_coefs all_test[]);
+void testing ();
 
 
 ///Функция вызываемая в main чтобы проверить, хочет ли пользователь провести тесты или же сразу решать уравнения
